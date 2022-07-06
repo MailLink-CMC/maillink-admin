d6aedfb4019250eae791aaf8b0362e0e117ca11d
Seongil-Shin <callmeshin75@gmail.com>
2022-07-06 23:42:22 +0900

    fix : functions auto

--- functions/api/auth.js
+++ functions/api/auth.js
@@ -1,18 +1,20 @@
 const axios = require('axios');
 const { Address } = require('./constants');
 
-exports.login = async (id, pw) => {
-  const res = await axios.post(`${Address}/api/v1/admin/login`, {
-    loginId: id,
-    password: pw,
-  });
-  await setTokens(res.data.data.token);
-};
-
-exports.setTokens = async (tokens) => {
+const setTokens = async (tokens) => {
   const tokensObj = {
     accessToken: tokens.accessToken,
     refreshToken: tokens.refreshToken,
   };
   axios.defaults.headers.common['Authorization'] = `Bearer ${tokensObj.accessToken}`;
 };
+
+exports.login = async (id, pw) => {
+  const res = await axios.post(`${Address}/api/v1/admin/login`, {
+    loginId: id,
+    password: pw,
+  });
+  setTokens(res.data.data.token);
+};
+
+exports.setTokens = setTokens;
